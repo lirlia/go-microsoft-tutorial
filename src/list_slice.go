d59b8768f8911d5196631993efeeab1d051dd44d
lirlia <lilria5000@gmail.com>
2021-03-23 23:36:35 +0900

    slice add

--- src/list_slice.go
+++ src/list_slice.go
@@ -24,4 +24,28 @@ func main() {
 		fmt.Println("Row", i, twoD[i])
 	}
 	fmt.Println("\nAll at once:", twoD)
+
+	// slice
+	months := []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+	fmt.Println(months)
+	fmt.Println("Length:", len(months))
+	fmt.Println("Capacity:", cap(months))
+
+	// cap()は対象の配列で指定されたポイントから最後までの個数
+	// ex
+	// months[3:6] なら
+	// len -> 3
+	// cap -> 9(3からはじまって12になる)
+	quarter1 := months[0:3]
+	quarter2 := months[3:6]
+	quarter3 := months[6:9]
+	quarter4 := months[9:12]
+	fmt.Println(quarter1, len(quarter1), cap(quarter1))
+	fmt.Println(quarter2, len(quarter2), cap(quarter2))
+	fmt.Println(quarter3, len(quarter3), cap(quarter3))
+	fmt.Println(quarter4, len(quarter4), cap(quarter4))
+	fmt.Println()
+	quarter2Extended := quarter2[:4]
+	fmt.Println(quarter2, len(quarter2), cap(quarter2))
+	fmt.Println(quarter2Extended, len(quarter2Extended), cap(quarter2Extended))
 }
